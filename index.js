cba85259d74e67bcfee91a444b0eaf857441fdee
OmarSanyang <59623844+OmarSanyang@users.noreply.github.com>
2022-09-29 14:47:44 -0400

    added routes

--- index.js
+++ index.js
@@ -20,6 +20,52 @@ app.get('/About', function(req, res){
   res.render('About')
 })
 
+// 'Interview1' Route
+app.get('/Interview1', function(req, res){
+  res.render('Interview1');
+})
+
+// 'Interview2' Route
+app.get('/Interview2', function(req, res){
+  res.render('Interview2');
+})
+
+// 'Interview3' Route
+app.get('/Interview3', function(req, res){
+  res.render('Interview3');
+})
+
+// 'Interview4' Route
+app.get('/Interview4', function(req, res){
+  res.render('Interview4');
+})
+
+// 'Interview5' Route
+app.get('/Interview5', function(req, res){
+  res.render('Interview5');
+})
+
+// 'personas' Route
+app.get('/personas', function(req, res){
+  res.render('personas');
+})
+
+// 'scenarios' Route
+app.get('/scenarios', function(req, res){
+  res.render('scenarios');
+})
+
+// 'user_stories' Route
+app.get('/user_stories', function(req, res){
+  res.render('user_stories');
+})
+
+// 'features' Route
+app.get('/features', function(req, res){
+  res.render('features');
+})
+
+
 
 // Start the server
 app.listen(port, function() {
